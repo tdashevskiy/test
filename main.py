fb31080895b94b5d46b4632d8f2e88827efc8cf0
Tatiana Dashevskiy <malasmsu@gmail.com>
2017-02-07 00:54:23 -0800

    Model building works Prediction works (though file name is hard coded) File uploading works (though buggy) 3D graphic generation works
    Improve file uploading and its usage

--- main.py
+++ main.py
@@ -1,134 +1,204 @@
-from flask import Flask, render_template, Markup, request
-
-import json
-import plotly
-from plotly.offline import plot
-import plotly.graph_objs as go
-
-import pandas as pd
+import os
 import numpy as np
+import json
+import pydotplus
+
+import plotly.graph_objs as go
+from plotly.offline import plot
+
+from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+from imblearn.over_sampling import SMOTE
+from scipy import stats
+from flask import Flask, render_template, Markup, jsonify, request, redirect, flash, url_for
+from werkzeug.utils import secure_filename
+
+# from plotly.tools import FigureFactory as FF
 
 app = Flask(__name__)
-app.debug = True
+
+UPLOAD_FOLDER = os.path.join( app.root_path, 'uploadedFiles' )
+ALLOWED_EXTENSIONS = set(['txt'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Set session secret key
+app.secret_key = 'some_secret'
+
+classifier = None
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash( 'No file part' )
+            return redirect( request.url )
+        file = request.files[ 'file' ]
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect( request.url )
+        if file and allowed_file( file.filename ) :
+            filename = secure_filename( file.filename )
+            file.save( os.path.join( app.config[ 'UPLOAD_FOLDER' ], filename ) )
+            flash( 'File uploaded!' )
+            # return redirect( url_for( 'uploaded_file', filename=filename ) )
+    return
+
+
+@app.route('/_predict')
+def _predict() :
+    inputdata = np.loadtxt( os.path.join( app.config[ 'UPLOAD_FOLDER' ], request.args['file'] ) )
+
+    global classifier
+    clf = classifier
+    Predicted_Crop = clf.predict(inputdata).tolist()
+
+    dictionary = { 1: 'Corn', 0: 'Not corn' }
+    res = map( lambda flag: dictionary[ flag ] + ' ', Predicted_Crop )
+
+    return jsonify( result = res )
+
+
+@app.route('/_buildModel')
+def _buildModel() :
+    res = classify()
+    global classifier
+    classifier = res[0]
+    return jsonify( score = res[1] )
+
+def classify() :
+    workingFolder = os.path.expanduser('~/fellowship_crops-master')
+    os.chdir(workingFolder)
+
+    data = np.load('grand_matrix.npy')
+    col_headers = np.load('col_headers.npy')
+
+    DATA = data[:, 3:]
+    Crop = data[:, 1]
+
+    Corn_ind = np.nonzero(Crop == 4)[0]
+    DATA_for_corn = np.zeros(len(Crop))
+    DATA_for_corn[Corn_ind] = 1
+
+    msk = np.random.rand(len(Crop)) < 0.8
+    trainDATA = DATA[msk, :]
+    testDATA = DATA[~msk, :]
+    trainCrop = DATA_for_corn[msk]
+    testCrop = DATA_for_corn[~msk]
+
+    # clf = skl.tree.DecisionTreeClassifier()
+    clf = RandomForestClassifier( n_estimators = 100 )
+    clf = clf.fit(trainDATA, trainCrop)
+    predict_data = clf.predict(testDATA)
+
+    dot_data = tree.export_graphviz(clf.estimators_[0], out_file=os.path.join(app.root_path, 'temp/Clf.dot'),
+                                    feature_names=col_headers[3:],
+                                    class_names=np.asarray(['corn', 'not corn']),
+                                    filled=True, rounded=True,
+                                    special_characters=True)
+
+    graph = pydotplus.graph_from_dot_file( os.path.join( app.root_path, 'temp/Clf.dot' ) )
+    pathToWrite = os.path.join(app.root_path, 'static/Clf.png')
+    graph.write_png(pathToWrite)
+
+    return clf, accuracy_score( testCrop, predict_data )
+
+
+@app.route('/_getKSDist')
+def _getKSDist( ) :
+    # inpt = json.loads( request.args.get( 'inpt' ) )
+    # do some stuff
+    # inpt = jsonify( result = inpt )
+
+    res = getKSDist()
+    x = res[0]
+    y = res[1]
+    z = res[2]
+
+    surface = go.Surface(x=x, y=y, z=z)
+    data = go.Data([surface])
+    layout = go.Layout(
+        scene=go.Scene(
+            xaxis=dict(range=[100, 350]),
+            yaxis=dict(range=[-0.2, 0.2])
+        ),
+        title='Probability Density of Vegetative Index vs. Day of the Year'
+    )
+    fig = go.Figure(data=data, layout=layout)
+
+    KSDist = plot(
+        fig
+        , output_type='div'
+        , include_plotlyjs=False
+        , show_link=False
+    )
+    KSDist = Markup( KSDist )
+
+    return jsonify( result = KSDist )
+
+
+def getKSDist( ) :
+    # figure KS distributions
+    workingFolder = os.path.expanduser('~/fellowship_crops-master')
+    os.chdir(workingFolder)
+
+    data = np.load('grand_matrix.npy')
+    # channels = np.load('channels.npy')
+    # col_headers = np.load('col_headers.npy')
+    # crop_dict = np.load('crop_dict.npy')
+
+    Corn_ind = np.nonzero(data == 4)[0]
+    data_corn = data[Corn_ind, :]
+
+    X = data_corn[:, 3]
+    Y = data_corn[:, 0]
+    Z = data_corn[:, 15]
+
+    z1 = X  # time
+    z2 = Z  # vegetative index
+
+    pdfxi = stats.gaussian_kde(z1)
+    xi = np.linspace(z1.min(), z1.max(), 100)
+    px = pdfxi(xi)
+
+    pdfyi = stats.gaussian_kde(z2)
+    yi = np.linspace(z2.min(), z2.max(), 100)
+    py = pdfyi(yi)
+
+    xii, yii = np.meshgrid(xi, yi)
+    pdfxii, pdfyii = np.meshgrid(px, py)
+
+    pdfxyi = pdfxii * pdfyii
+
+    m1 = np.meshgrid(xi, yi, pdfxyi)
+    return xii, yii, pdfxyi
+
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    x, y = np.mgrid[-2 * np.pi:2 * np.pi:300j, -2:2:300j]
-
-    # x = np.linspace(0, 10, 10)
-    # y = np.linspace(0, 10, 10)
-    # xGrid, yGrid = np.meshgrid(x, y)
-    # userinput1 = 1  # Want user to input these variables
-    # userinput2 = 2
-    # userinput3 = 3
-    # zGrid = lambda x1, y1: (x1 ** userinput1 + y1 ** userinput2) + userinput3 ** userinput3
-    # surface = go.Surface(x=xGrid, y=yGrid, z=zGrid(xGrid, yGrid))
-    # data = go.Data([surface])
-    # fig = go.Figure(data=data)
-
-    div_test = plot(
-        # fig,
-        {
-            "data": [
-                go.Surface(
-                    x=x,
-                    y=y,
-                    z=-np.cos(x)+y**2/2
-                )
-            ],#go.Scatter(x=[1, 2, 3], y=[3, 1, 6])],
-            "layout": go.Layout( title = 'Hello World' ) # Surface, Scene
-        },
-        output_type = 'div',
-        include_plotlyjs = False,
-        show_link = False
-        # ,image_height = 400
-        # ,image_width = 400
-    )
-
-    # result = 0
-    # error = ''
-    # # you may want to customize your GET... in this case not applicable
-    # if request.method=='POST':
-    #     # get the form data
-    #     first = request.form['first']
-    #     second = request.form['second']
-    #     if first and second:
-    #         try:
-    #             # do your validation or logic here...
-    #             if int(first)>10 or int(first)<1:
-    #                 raise ValueError
-    #             result = int(first) + int(second)
-    #         except ValueError:
-    #             # you may pass custom error message as you like
-    #             error = 'Please input integer from 1-10 only.'
-    # # you render the template and pass the context result & error
-
-    rng = pd.date_range('1/1/2011', periods=7500, freq='H')
-    ts = pd.Series(np.random.randn(len(rng)), index=rng)
-
-    graphs = [
-        dict(
-            data=[
-                dict(
-                    x=[1, 3, 5],
-                    y=[10, 50, 30],
-                    type='bar'
-                ),
-            ],
-            layout=dict(
-                title='second graph'
-            )
-        ),
-        dict(
-            data=[
-                dict(
-                    x=ts.index,  # Can use the pandas data structures directly
-                    y=ts
-                )
-            ]
-        )
-        # ,dict(
-        #     data=[
-        #         dict(
-        #             x=[1, 2, 3],
-        #             y=[10, 20, 30],
-        #             type='scatter'
-        #         ),
-        #     ],
-        #     layout=dict(
-        #         title='first graph'
-        #     )
-        # )
-    ]
-
-    # Add "ids" to each of the graphs to pass up to the client
-    # for templating
+    graphs = []
     ids = ['Tab{}Content'.format(i) for i, _ in enumerate(graphs)]
 
-    # Convert the figures to JSON
-    # PlotlyJSONEncoder appropriately converts pandas, datetime, etc
-    # objects to their JSON equivalents
-    graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
-    print( ids )
-
     return render_template('index.html',
-                           ids = ids,
-                           graphJSON = graphJSON,
-                           div_test = Markup( div_test )
+                           ids = ids
                            )
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run( debug = True, use_reloader = True)
     #app.run(host='0.0.0.0', port=9999)
 
-# def index():
-#     return '<dd><input type=submit value=Load images>'
-#
 # https://stormpath.com/blog/build-a-flask-app-in-30-minutes
 # http://hplgit.github.io/web4sciapps/doc/pub/._web4sa_flask017.html#___sec64
 # https://realpython.com/blog/python/flask-by-example-updating-the-ui/
 # http://hplgit.github.io/parampool/doc/pub/._pp003.html
 # https://codepen.io/plotly/pres/wKpPvj
-# http://stackoverflow.com/questions/40595002/plotly-js-responsive-graph-in-div
-# https://plot.ly/javascript/responsive-fluid-layout/
+
--- static/logic.js
+++ static/logic.js
@@ -1,24 +1,116 @@
-function openTab(evt, tabName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
-
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
+window.onresize = function() {
+    gd = Plotly.d3.select('#Tab2-contents > div').node();
+    if ( gd ){
+        Plotly.Plots.resize( gd );
     }
+};
 
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-
-    // Show the current tab, and add an "active" class to the link that opened the tab
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
+function performClick(elemId) {
+   var elem = document.getElementById(elemId);
+   if(elem && document.createEvent) {
+      var evt = document.createEvent("MouseEvents");
+      evt.initEvent("click", true, false);
+      elem.dispatchEvent(evt);
+   }
 }
 
-window.onresize = function() {
-    Plotly.Plots.resize(gd);
+$body = $("body");
+$(document).on({
+    ajaxStart: function() { $body.addClass("loading");    },
+    ajaxStop:  function() { $body.removeClass("loading"); }
+});
+
+$( function() {
+    var tabs = $( "#tabs" ).tabs();
+    tabs.find( ".ui-tabs-nav" ).sortable({
+        axis: "x",
+        stop: function() {
+            tabs.tabs( "refresh" );
+        }
+    });
+});
+
+
+$( function() {
+    $( "#tabs input[type=submit]" ).button();
+});
+
+
+$( function() {
+    $( ".btnLoad" ).on( "click", function( event ) {
+        var target = $( event.target );
+        $body.addClass("loading");
+    });
+});
+
+// ============================================================= //
+
+$(document).ready(function() {
+    $( '#Tab0 input' ).bind( 'click', function(){
+        return false;
+    });
+
+    $( '#buildModel' ).bind( 'click', function(){
+        buildModel();
+        return false;
+    });
+
+    $( '#predict' ).bind( 'click', function(){
+        predict('miniset.txt');
+        return false;
+    });
+
+    $( '#Tab2 input' ).bind( 'click', function(){
+        getKSDist();
+        return false;
+    });
+
+    $( '#upload' ).bind( 'click', function(){
+        $('#form').submit();
+        return false;
+    });
+});
+
+function getKSDist( inpt ){
+    $.getJSON( $SCRIPT_ROOT + '/_getKSDist', {
+        inpt: JSON.stringify( 'tesT' )
+    }, function( data ){
+        var WIDTH_IN_PERCENT_OF_PARENT = 100,
+        HEIGHT_IN_PERCENT_OF_PARENT = 100;
+
+        $('#Tab2-contents').empty();
+        $('#Tab2-contents').addClass('hidden');
+        $('#Tab2-contents').append( data.result );
+        $('#Tab2-contents > div').css({
+            width: WIDTH_IN_PERCENT_OF_PARENT + '%',
+            'margin-left': (100 - WIDTH_IN_PERCENT_OF_PARENT) / 2 + '%',
+            height: HEIGHT_IN_PERCENT_OF_PARENT + 'vh',
+            'margin-top': (100 - HEIGHT_IN_PERCENT_OF_PARENT) / 2 + 'vh'
+        });
+        Plotly.Plots.resize( Plotly.d3.select('#Tab2-contents > div').node() );
+        $('#Tab2-contents').removeClass('hidden');
+
+        $body.removeClass("loading");
+    });
+};
+
+function buildModel(){
+    $.getJSON( $SCRIPT_ROOT + '/_buildModel', {
+    }, function( data ){
+        alert( 'Accuracy score: ' + data.score )
+        $('<img src="static/Clf.png">').load(function() {
+            $(this).width('3000px').appendTo('#tree');
+        });
+
+        $body.removeClass("loading");
+    })
+};
+
+function predict( input ){
+    $.getJSON( $SCRIPT_ROOT + '/_predict', {
+        file: input
+    }, function( data ){
+        $( '#predictResult' ).append( data.result );
+        $body.removeClass("loading");
+    })
 };
